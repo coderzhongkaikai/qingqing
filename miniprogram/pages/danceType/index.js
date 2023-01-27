496e8e29807cdee4a2b77c12f242f97649fc29c6
coderzhongkaikai <2457169144@qq.com>
2023-01-28 00:32:23 +0800

    x

--- miniprogram/pages/danceType/index.js
+++ miniprogram/pages/danceType/index.js
@@ -1,6 +1,13 @@
 // pages/danceType/index.js
 const dance_type=['韩舞' ,'街舞', '爵士舞','芭蕾舞', '钢管舞']
 const dance_type_img=['hanwu.jpeg','jiewu.jpeg','jueshi.jpeg','balei.jpeg','gangguan.jpeg']
+const dance_desc=[
+  '一般韩国的流行舞统称为韩舞（K-POPDANCE），也会被称为K-POP或MV流行舞。',
+  '来自美国纽约的嘻哈舞蹈。舞姿自由灵动，张扬青春个性。',
+  '爵士舞（Jazz Dance），爵士舞是一种充满活力与创造性的个人即兴表演。',
+  '芭蕾舞是一种欧洲古典舞蹈，孕育于意大利文艺复兴时期，十七世纪后半叶开始在法国发展流行并逐渐职业化，在不断革新中风靡世界。',
+  '钢管舞（Pole dance），是利用钢管为道具，综合爵士舞、现代舞、民族舞、芭蕾舞、瑜伽、肚皮舞、拉丁舞等各种不同风格舞种，又集合杂技、艺术体操、健身类别的运动而衍生出来的新型舞蹈。'
+]
 Page({
 
   /**
@@ -8,7 +15,8 @@ Page({
    */
   data: {
     dance_type_img,
-    dance_type
+    dance_type,
+    dance_desc
   },
 
   /**
